e8d9a2e9ebb0c523b46fdea1c122e1d87e72a3c5
Achchala Deepan <achchala2003@gmail.com>
2024-02-17 22:55:52 -0500

    added document to users table!!!

--- app.py
+++ app.py
@@ -6,13 +6,20 @@ client = MongoClient(
 )
 
 # Select database
-db = client["test_database"]
+db = client["unifyDB"]
 
 # Select collection
-collection = db["test_collection"]
+collection = db["users"]
 
 # Create a document to insert
-document = {"name": "John Doe", "age": 30, "city": "New York"}
+document = {
+    "name": "John Doe",
+    "email": "john.doe@example.com",
+    "password": "password123",
+    "university": "University",
+    "program": "Program",
+    "courses": ["Course 1", "Course 2"],
+}
 
 # Insert document into collection
 inserted_document = collection.insert_one(document)
